ea97888ad7813a19689a52bcaaa05c900ef3c551
kokic <3250679033@qq.com>
2022-08-14 00:31:08 +0800

    map

--- torato.ml
+++ torato.ml
@@ -10,7 +10,17 @@ Random.self_init () ;;
 
 let alphabets = explode "abcdefghijklmnopqrstuvwxyz" ;;
 
+module AdjacentOrd = struct
+  type t = char * (char list)
+  let compare (s, t) (u, v) = match Stdlib.compare s u with
+    0 -> Stdlib.compare t v | _ -> _
+end
+
+module Adjacents = Map.Make (AdjacentOrd) ;;
+
 let randomVisit xs = List.nth xs (Random.int (List.length xs)) ;;
+let next adjacents peek = peek ;;
+
 
 for index = 0 to 20 do
   printf "%c " (randomVisit alphabets)
