3672e738e324be01ffa5484aa64cd67112fe5002
kokic <3250679033@qq.com>
2022-08-13 14:38:06 +0800

    #

--- algebraic-arithmetic.hs
+++ algebraic-arithmetic.hs
@@ -26,20 +26,17 @@ paren = bundle "(" ")"
 brace = bundle "{" "}"
 
 instance Prettify Rational where
-  prettify x = delta (q > 1) (show p  ++ '/' : show q) (show p)
+  prettify x = delta (q > 1) (show p  ++ '/' : show q) (paren p)
     where (p, q) = (numerator x, denominator x)
-    
-  toTex x = delta (q > 1) ("\\frac" ++ brace p ++ brace q) (show p)
+  toTex x = delta (q > 1) ("\\frac" ++ brace p ++ brace q) (brace p)
     where (p, q) = (numerator x, denominator x)
 
 instance Prettify OneVariableMonomial where
-  prettify x = (prettify (coefficient x)) ++ (variable x) ++ '^' : 
-               (prettify (power x))
+  prettify x = (prettify (coefficient x)) ++ (variable x) ++ '^' : (prettify (power x))
+  toTex x = (toTex (coefficient x)) ++ (variable x) ++ '^' : (toTex (power x))
 
-  toTex x = (toTex (coefficient x)) ++ (variable x) ++ (prettify (power x))
 
-x = OneVariableMonomial "x" (7 / (-3)) (-1)
-
+x = OneVariableMonomial "x" (7 / (-3)) 5
 main = putStrLn $ prettify x ++ "\n" ++ toTex x
 
 
