f7f6894c0f2ec4165e04f6eafc970b313dce8f82
kokic <3250679033@qq.com>
2022-08-14 00:00:43 +0800

    random

--- torato.ml
+++ torato.ml
@@ -1,5 +1,23 @@
 
+open List
+open Printf
+
 let delta condition x y = match condition with true -> x | _ -> y ;;
+
+let explode s = init (String.length s) (String.get s) ;;
+
+Random.self_init () ;;
+
+let alphabets = explode "abcdefghijklmnopqrstuvwxyz" ;;
+
+let randomVisit xs = List.nth xs (Random.int (List.length xs)) ;;
+
+for index = 0 to 20 do
+  printf "%c " (randomVisit alphabets)
+done ;;
+
+
+(*
 
 (* quasi ad-hoc polymorphism in ocaml *)
 
@@ -12,3 +30,4 @@ let ((+), _) = add_polymorphism ;;
 print_int (1 + 3) ;;
 print_newline () ;;
 
+*)
