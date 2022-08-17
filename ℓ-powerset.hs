d61b21245a4f4fb7a3362f0c5672a57cf907e224
kokic <3250679033@qq.com>
2022-08-17 22:10:41 +0800

    ℓ^X to ℓ

--- ℓ-powerset.hs
+++ ℓ-powerset.hs
@@ -22,20 +22,35 @@ manifold𝟙 f xs = manifold f xs id
 ℓ_powerset ℓ u | ℓ >= 3 = foldr (++) [] succ
   where succ = [ ℓ_powerset 2 sublist | sublist <- pred ]
         pred = ℓ_powerset (ℓ - 1) u 
+
 ℓ_powerset ℓ u | ℓ == 2 = [[ fst pair | pair <- xs ] 
     | subset <- redundance, let xs = Set.toList subset ]
   where redundance = Set.toList (Set.powerSet uniqueized)
         uniqueized = Set.fromList (indexed u)
+-- theorem. 1^X = {X}
 ℓ_powerset ℓ u | ℓ == 1 = [u]
 -- proof. (1) 2^X = ∪ 2^Y where Y ∈ 1^X 
 --        (2) |1^X| = 1^|X| = 1, so 2^X = 2^Y, X = Y
 --        (3) in summary, 1^X = {X}
 
+
 -- by the definition, this can also be understood as 1^∅
 𝜔1 = Set.singleton Set.empty -- {∅}
 
+
+-- definition. order γ for X := |{x ∈ X : x = γ }| i.e. number of γ
 order :: (Eq a) => [a] -> a -> Int
+order xs x | xs == [] = 0
 order xs x = foldl' (\ s t -> s + delta (t == x) 1 0) 0 xs
+
+-- theorem. (a) order  ∅  for (ℓ+1)^X = ℓ^2
+--          (b) order {x} for (ℓ+1)^X, ∀ x ∈ X = ℓ
+--          (c) order  X  for (ℓ+1)^X = 1
+
+-- proof. ...
+ 
+-- theorem. if S = ℓ^X, then ℓ = 1 + order {x} for S, ∀ x ∈ X
+--       ⟺ ℓ = 1 + sum 1 for Y ∈ ℓ^X, |Y| = 1
 
 
 -- prettify 
