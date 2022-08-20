c89a4ad9345261969c7083d18a8a3afcd59fa2ee
kokic <3250679033@qq.com>
2022-08-20 20:43:19 +0800

    perface of demangle

---
+++ demangle.ml
@@ -0,0 +1,147 @@
+
+(*
+                                    Perface
+Inspiration by: 
+  - https://android.googlesource.com/platform/external/gcc-demangle/+/
+       d6f70187df96e4f1968adbe9b52035dc474b7590/cp-demangle.c
+  - https://opensource.apple.com/source/gcc/gcc-5482/libiberty/cp-demangle.c.auto.html
+
+Reference:
+  - https://fitzgeraldnick.com/2017/02/22/cpp-demangle.html (in Rust)
+  - https://itanium-cxx-abi.github.io/cxx-abi/abi.html (Itanium C++ ABI’s name mangling rules)
+
+That's all, thanks! 
+*)
+
+(*
+Builtin types
+
+  <builtin-type> ::= v	# void
+		 ::= w	# wchar_t
+		 ::= b	# bool
+		 ::= c	# char
+		 ::= a	# signed char
+		 ::= h	# unsigned char
+		 ::= s	# short
+		 ::= t	# unsigned short
+		 ::= i	# int
+		 ::= j	# unsigned int
+		 ::= l	# long
+		 ::= m	# unsigned long
+		 ::= x	# long long, __int64
+		 ::= y	# unsigned long long, __int64
+		 ::= n	# __int128
+		 ::= o	# unsigned __int128
+		 ::= f	# float
+		 ::= d	# double
+		 ::= e	# long double, __float80
+		 ::= g	# __float128
+		 ::= z	# ellipsis
+     ::= Dd # IEEE 754r decimal floating point (64 bits)
+     ::= De # IEEE 754r decimal floating point (128 bits)
+     ::= Df # IEEE 754r decimal floating point (32 bits)
+     ::= Dh # IEEE 754r half-precision floating point (16 bits)
+     ::= DF <number> _ # ISO/IEC TS 18661 binary floating point type _FloatN (N bits)
+     ::= DB <number> _        # C23 signed _BitInt(N)
+     ::= DB <instantiation-dependent expression> _ # C23 signed _BitInt(N)
+     ::= DU <number> _        # C23 unsigned _BitInt(N)
+     ::= DU <instantiation-dependent expression> _ # C23 unsigned _BitInt(N)
+     ::= Di # char32_t
+     ::= Ds # char16_t
+     ::= Du # char8_t
+     ::= Da # auto
+     ::= Dc # decltype(auto)
+     ::= Dn # std::nullptr_t (i.e., decltype(nullptr))
+		 ::= u <source-name> [<template-args>] # vendor extended type
+*)
+
+(*
+                       Type encodings
+
+  <type> ::= <builtin-type>
+         ::= <qualified-type>
+         ::= <function-type>
+         ::= <class-enum-type>
+         ::= <array-type>
+         ::= <pointer-to-member-type>
+         ::= <template-param>
+         ::= <template-template-param> <template-args>
+         ::= <decltype>
+         ::= P <type>        # pointer
+         ::= R <type>        # l-value reference
+         ::= O <type>        # r-value reference (C++11)
+         ::= C <type>        # complex pair (C99)
+         ::= G <type>        # imaginary (C99)
+         ::= <substitution>  # See Compression below
+*)
+
+(* 
+    Operator Encodings:
+
+<operator-name> ::= nw	# new           
+		  ::= na	# new[]
+		  ::= dl	# delete        
+		  ::= da	# delete[]      
+		  ::= aw	# co_await      
+		  ::= ps  # + (unary)
+		  ::= ng	# - (unary)     
+		  ::= ad	# & (unary)     
+		  ::= de	# * (unary)     
+		  ::= co	# ~             
+		  ::= pl	# +             
+		  ::= mi	# -             
+		  ::= ml	# *             
+		  ::= dv	# /             
+		  ::= rm	# %             
+		  ::= an	# &             
+		  ::= or	# |             
+		  ::= eo	# ^             
+		  ::= aS	# =             
+		  ::= pL	# +=            
+		  ::= mI	# -=            
+		  ::= mL	# *=            
+		  ::= dV	# /=            
+		  ::= rM	# %=            
+		  ::= aN	# &=            
+		  ::= oR	# |=            
+		  ::= eO	# ^=            
+		  ::= ls	# <<            
+		  ::= rs	# >>            
+		  ::= lS	# <<=           
+		  ::= rS	# >>=           
+		  ::= eq	# ==            
+		  ::= ne	# !=            
+		  ::= lt	# <             
+		  ::= gt	# >             
+		  ::= le	# <=            
+		  ::= ge	# >=            
+		  ::= ss	# <=>           
+		  ::= nt	# !             
+		  ::= aa	# &&            
+		  ::= oo	# ||            
+		  ::= pp	# ++ (postfix in <expression> context)
+		  ::= mm	# -- (postfix in <expression> context)           
+		  ::= cm	# ,             
+		  ::= pm	# ->*           
+		  ::= pt	# ->            
+		  ::= cl	# ()            
+		  ::= ix	# []            
+		  ::= qu	# ?             
+		  ::= cv <type>	# (cast)
+      ::= li <source-name>        # operator ""
+		  ::= v <digit> <source-name>	# vendor extended operator
+*)
+
+(*                 
+               Virtual Tables and RTTI
+
+<special-name> ::= TV <type>	# virtual table
+		 ::= TT <type>	# VTT structure (construction vtable index)
+		 ::= TI <type>	# typeinfo structure
+		 ::= TS <type>	# typeinfo name (null-terminated byte string)
+*)
+
+
+
+
+
