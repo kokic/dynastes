a56ec2b3f5c8cf98b2cd2d1ec1b445b930ddc47e
kokic <3250679033@qq.com>
2022-08-25 10:49:18 +0800

    Num :: OneVariableMonomial

--- algebraic-arithmetic.hs
+++ algebraic-arithmetic.hs
@@ -8,14 +8,9 @@ delta :: Bool -> a -> a -> a
 delta True  x _ = x
 delta False _ y = y
 
-data OneVariableMonomial = OneVariableMonomial {
-  variable :: String, 
-  coefficient :: Rational,
-  power :: Rational
-} deriving (Eq)
-
-type Monomial = [OneVariableMonomial]
-type Polynomial = [Monomial]
+bundle left right n = delta (occup n > 1) (left ++ s ++ right) s where s = show n
+paren = bundle "(" ")"
+brace = bundle "{" "}"
 
 occup :: Integer -> Int
 occup x = length (show x)
@@ -23,10 +18,6 @@ occup x = length (show x)
 class Prettify a where 
   prettify :: a -> String
   toTex :: a -> String
-
-bundle left right n = delta (occup n > 1) (left ++ s ++ right) s where s = show n
-paren = bundle "(" ")"
-brace = bundle "{" "}"
 
 instance Prettify Rational where
   prettify x | x == 1 = [] | q == 1 = paren p
@@ -36,22 +27,50 @@ instance Prettify Rational where
     | otherwise = "\\frac" ++ brace p ++ brace q
     where (p, q) = (numerator x, denominator x)
 
+
+
+data OneVariableMonomial = OneVariableMonomial {
+  variable :: String, 
+  coefficient :: Rational,
+  power :: Rational
+} deriving (Eq)
+
+oneMonomial variable coefficient power = OneVariableMonomial variable coefficient power
+oneMonomialX = oneMonomial "x"
+oneMonomialC coefficient = oneMonomial "" coefficient 0
+
+type Monomial = [OneVariableMonomial]
+type Polynomial = [Monomial]
+
+prettifyPowerF :: String -> Rational -> String
+prettifyPowerF _ 0 = []
+prettifyPowerF variable 1 = variable
+prettifyPowerF variable power = variable ++ '^' : (prettify power)
+
+toTexPowerF :: String -> Rational -> String
+toTexPowerF _ 0 = []
+toTexPowerF variable 1 = variable
+toTexPowerF variable power = variable ++ '^' : (toTex power)
+
 instance Prettify OneVariableMonomial where
-  prettify x = (prettify (coefficient x)) ++ (variable x) ++ '^' : (prettify (power x))
-  toTex x = (toTex (coefficient x)) ++ (variable x) ++ '^' : (toTex (power x))
-
-
--- data Polynomial = OneVariableMonomialCase OneVariableMonomial 
+  prettify x = (prettify (coefficient x)) ++ prettifyPowerF (variable x) (power x)
+  toTex x = (toTex (coefficient x)) ++ toTexPowerF (variable x) (power x)
 
 instance Num OneVariableMonomial where
   (+) x y = OneVariableMonomial "x" (coefficient x + coefficient y) (power x)
+  (-) x y = OneVariableMonomial "x" (coefficient x - coefficient y) (power x)
+  (*) x y = OneVariableMonomial "x" (coefficient x * coefficient y) (power x)
+  abs x = OneVariableMonomial "x" (abs (coefficient x)) (power x)
+  signum x = oneMonomialC (abs (coefficient x))
+  fromInteger x = oneMonomialC (fromIntegral x)
 
+x = oneMonomialX (7 / (-3)) 5
+y = oneMonomialX 2 5
 
-
-x = OneVariableMonomial "x" (7 / (-3)) 5
-y = OneVariableMonomial "x" 2 5
-
-main = putStrLn $ prettify z ++ "\n" ++ toTex z where z = x + y
+main = putStrLn $ prettify z ++ "\n" ++ 
+  toTex z ++ "\n" ++
+  prettify (signum z) 
+  where z = x + y
 
 
 
